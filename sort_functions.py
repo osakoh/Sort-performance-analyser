81dd825c663562b7dcb6f09db69e13786227fdfd
Osas Michael <macmive@gmail.com>
2020-12-28 18:27:08 +0000

    made copy of array in bubble & insertion sort

--- sort_functions.py
+++ sort_functions.py
@@ -1,103 +1,106 @@
 import random
 
 
-class Sort:
+def bubble_sort(arr):
+    c_arr = arr.copy()
+    swap = True  # so it runs at least once
 
-    def __init__(self):
-        self.swap = True  # so it runs at least once
+    while swap:
+        swap = False  # prevents the loop doesn't run infinitely
 
-    def bubble_sort(self, arr):
-        while self.swap:
-            self.swap = False  # prevents the loop doesn't run infinitely
+        for num in range(0, len(c_arr) - 1):
+            if c_arr[num] > c_arr[num + 1]:
+                swap = True
+                c_arr[num], c_arr[num + 1] = c_arr[num + 1], c_arr[num]
+    return c_arr
 
-            for num in range(0, len(arr) - 1):
-                if arr[num] > arr[num + 1]:
-                    self.swap = True
-                    arr[num], arr[num + 1] = arr[num + 1], arr[num]
-        return arr
 
-    def insertion_sort(self, arr):
+def insertion_sort(arr):
+    c_arr = arr
+    swap = True  # so it runs at least once
 
-        while self.swap:
-            self.swap = False  # prevents the loop doesn't run infinitely
-            for index in range(1, len(arr)):
-                # arr[index]- the current value in the array
-                current = arr[index]
-                # index - 1 because the previous line makes current to be a value in the array rather than an index
-                previous = index - 1
+    while swap:
+        swap = False  # prevents the loop doesn't run infinitely
+        for index in range(1, len(c_arr)):
+            # arr[index]- the current value in the array
+            current = c_arr[index]
+            # index - 1 because the previous line makes current to be a value in the array rather than an index
+            previous = index - 1
 
-                if current < arr[previous]:
-                    self.swap = True
-                    # shift the larger number to its right and the smaller number to previous
-                    arr[previous + 1], arr[previous] = arr[previous], current
+            if current < c_arr[previous]:
+                swap = True
+                # shift the larger number to its right and the smaller number to previous
+                c_arr[previous + 1], c_arr[previous] = c_arr[previous], current
 
-                current = index + 1
-                previous = current - 1
+            current = index + 1
+            previous = current - 1
 
-        return arr
+    return c_arr
 
-    def merge(self, arr1, arr2):
-        """
-        first and second list are sorted in ascending order
-        function returns a sorted merged list
-        """
-        i = j = 0
-        merged_list = []
 
-        while i < len(arr1) and j < len(arr2):
-            if arr1[i] < arr2[j]:
-                merged_list.append(arr1[i])
-                i += 1
-            else:
-                merged_list.append(arr2[j])
-                j += 1
+def merge(arr1, arr2):
+    """
+    first and second list are sorted in ascending order
+    function returns a sorted merged list
+    """
+    i = j = 0
+    merged_list = []
 
-        while i < len(arr1):
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]:
             merged_list.append(arr1[i])
             i += 1
-
-        while j < len(arr2):
+        else:
             merged_list.append(arr2[j])
             j += 1
 
-        return merged_list
+    while i < len(arr1):
+        merged_list.append(arr1[i])
+        i += 1
 
-    def divide_and_merge(self, arr):
-        # base case for recursion
-        if len(arr) < 2:  # 1 element
-            return arr
+    while j < len(arr2):
+        merged_list.append(arr2[j])
+        j += 1
 
+    return merged_list
+
+
+def merge_sort(arr):
+    """
+    takes an array; keep dividing the arr until it remains one item in the arr and returns it
+    returns a sorted array
+    """
+    # base case for recursion
+    if len(arr) < 2:  # 1 element
+        return arr
+
+    else:
+        midpoint = len(arr) // 2  # integer division
+        left = arr[:midpoint]
+        right = arr[midpoint:]
+
+        l1 = merge_sort(left)
+        l2 = merge_sort(right)
+    return merge(l1, l2)
+
+
+def quick_sort(arr):
+    # base case
+    if len(arr) < 1:
+        return arr
+
+    pivot = random.randint(0, len(arr) - 1)
+    left, middle, right = [], [], []
+
+    for num in range(0, len(arr)):
+        if arr[num] < arr[pivot]:
+            left.append(arr[num])
+        elif arr[num] == arr[pivot]:
+            middle.append(arr[num])
         else:
-            midpoint = len(arr) // 2  # integer division
-            left = arr[:midpoint]
-            right = arr[midpoint:]
-
-            l1 = self.divide_and_merge(left)
-            l2 = self.divide_and_merge(right)
-        return self.merge(l1, l2)
-
-    def quick_sort(self, arr):
-        # base case
-        if len(arr) < 1:
-            return arr
-
-        pivot = random.randint(0, len(arr) - 1)
-        left, middle, right = [], [], []
-
-        for num in range(0, len(arr)):
-            if arr[num] < arr[pivot]:
-                left.append(arr[num])
-            elif arr[num] == arr[pivot]:
-                middle.append(arr[num])
-            else:
-                right.append(arr[num])
-        # return left, middle, right
-        return self.quick_sort(left) + middle + self.quick_sort(right)
+            right.append(arr[num])
+    # return left, middle, right
+    return quick_sort(left) + middle + quick_sort(right)
 
 
-s = Sort()
-l = [-1, 16, 85, 0, 12, 34, 60, 70, 8, 9, 3, 2, 5]
-print(f"Bubble sort:    {s.bubble_sort(l)}")
-print(f"Insertion sort: {s.insertion_sort(l)}")
-print(f"Merge sort:     {s.divide_and_merge(l)}")
-print(f"Quick sort:     {s.quick_sort(l)}")
+# print(quick_sort([29, 99, 27, 41, 66, 28, 44, 78, 87, 19, 31, 76, 58, 88, 83, 97, 12, 21, 44]))
