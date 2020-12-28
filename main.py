be30cd73b9cf0b8bfcbb6f35e95ae374401788ac
Osas Michael <macmive@gmail.com>
2020-12-28 20:38:54 +0000

    finish menu function

--- main.py
+++ main.py
@@ -1,49 +1,57 @@
-from sort_analyser import random_list_generator, measure_runtime
+from sort_analyser import random_list_generator, measure_runtime, select_function
 from sort_functions import *
 
 
 def menu():
-    print("\t\t|--- Welcome to my sorting algorithm analyser ---|\n")
-    menu_options()
+    print("-------------------------- Welcome to my sorting algorithm analyser --------------------------|\n")
+    select_options()
 
     user_input = int(input("Enter option: "))
-    size = int(input("Enter the size of list to be created: "))
+    size = int(input("\nEnter the size of list to be created: "))
     maximum_num = int(input("Enter the maximum number to be created in the list: "))
-
-    gen_list = random_list_generator(size, maximum_num)
+    print()
+    run = select_function(user_input)  # returns a sort function object
 
     while user_input != 0:
-        if user_input == 1:  #
-            print(user_input)
+        if user_input == 1:  # bubble sort
+            gen_list = random_list_generator(size, maximum_num)  # generates array
+            return measure_runtime(run, gen_list)
 
-        elif user_input == 2:  #
-            print(user_input)
+        elif user_input == 2:  # insertion sort
+            gen_list = random_list_generator(size, maximum_num)  # generates array
+            return measure_runtime(run, gen_list)
 
-        elif user_input == 3:  #
-            print(user_input)
+        elif user_input == 3:  # merge sort
+            gen_list = random_list_generator(size, maximum_num)  # generates array
+            return measure_runtime(run, gen_list)
 
-        elif user_input == 4:  # delete a movie
-            print(user_input)
+        elif user_input == 4:  # quick sort
+            gen_list = random_list_generator(size, maximum_num)  # generates array
+            return measure_runtime(run, gen_list)
 
-        elif user_input == 5:  # show watched movies
-            print(user_input)
+        elif user_input == 5:  # python's sorting function
+            gen_list = random_list_generator(size, maximum_num)  # generates array
+            return measure_runtime(run, gen_list)
 
         elif user_input == 6:  # save user profile
-            print(user_input)
+            gen_list = random_list_generator(size, maximum_num)  # generates array
+            for functions in run:
+                res = measure_runtime(functions, gen_list)
+            return res
 
-        menu_options()  # display a list of options for the user to choose from
-        user_input = int(input("Select from the menu: "))
+        select_options()  # display a list of options for the user to choose from
+        user_input = int(input("\nSelect from the menu: "))
+        gen_list = random_list_generator(size, maximum_num)  # generates array
 
 
-def menu_options():
+def select_options():
     print("____________________________________\n"
-          "¦____ Movie Rental Application ____¦\n"
-          "| 1.     Add a movie               |\n"
-          "| 2.     View list of movies       |\n"
-          "| 3.     Set a movie as watched    |\n"
-          "| 4.     Delete a movie            |\n"
-          "| 5.     View watched movies       |\n"
-          "| 6.     Save profile              |\n"
+          "| 1.     Bubble sort               |\n"
+          "| 2.     Insertion sort            |\n"
+          "| 3.     Merge sort                |\n"
+          "| 4.     Quick Sort                |\n"
+          "| 5.     Python's sorted Function  |\n"
+          "| 6.     Run all function          |\n"
           "| 0.     Exit application          |\n"
           "|__________________________________|")
 
