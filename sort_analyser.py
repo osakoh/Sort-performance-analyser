594f2f288973bf3583892f12c84c56ed6a2099d1
Osas Michael <macmive@gmail.com>
2020-12-28 18:26:17 +0000

    implemented select_options

--- sort_analyser.py
+++ sort_analyser.py
@@ -1,4 +1,7 @@
 import random
+import time
+
+from sort_functions import *
 
 
 def random_list_generator(size, max_num):
@@ -14,4 +17,49 @@ def random_list_generator(size, max_num):
     return [random.randint(1, max_num) for _ in range(size)]
 
 
-print(random_list_generator(5, 100))
+def select_options():
+    print("____________________________________\n"
+          "| 1.     Bubble sort               |\n"
+          "| 2.     Insertion sort            |\n"
+          "| 3.     Merge sort                |\n"
+          "| 4.     Quick Sort                |\n"
+          "| 5.     Python's sorted Function  |\n"
+          "| 6.     Run all function          |\n"
+          "| 0.     Exit application          |\n"
+          "|__________________________________|")
+
+
+def select_function(choice):
+    select_options()
+    choice = int(input("Select algorithm to run: "))
+
+    if choice == 1:
+        return bubble_sort
+    elif choice == 2:
+        return insertion_sort
+    elif choice == 3:
+        return merge_sort
+    elif choice == 4:
+        return quick_sort
+    elif choice == 5:
+        return sorted
+    elif choice == 6:
+        temp = [bubble_sort, insertion_sort, merge_sort, quick_sort, sorted]
+        return temp
+    else:
+        exit()
+
+
+def measure_runtime(func, arr):
+    """
+    :param func: function object without the brackets, i.e the function is not being called
+    :param arr:
+    :return:
+    """
+    start = time.time()
+    func(arr)
+    end = time.time()
+    fn = func.__name__.replace("_", " ").title()
+    print(f"\nRuntime for {fn}: {end - start} seconds")
+
+
